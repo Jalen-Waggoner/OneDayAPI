1da69e211b642ce3fbe126887a27fb8319814728
Floria Karamdad <floriakaramdad2022@gmail.com>
2022-07-12 16:10:43 -0400

    Floria Entity added

--- OneDay.Data/ApplicationDbContext.cs
+++ OneDay.Data/ApplicationDbContext.cs
@@ -4,6 +4,6 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        
+        public DbSet<Floria Entity> Florias {get; set; }
     }
 }
