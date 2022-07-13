70a5c31a77fc956e9ce1040754bf25e2514d22e2
Floria Karamdad <floriakaramdad2022@gmail.com>
2022-07-12 23:05:33 -0400

    more work

---
+++ OneDay.Services/IFloriaService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneDay.Services
+{
+    public class IFloriaService
+    {
+        
+    }
+}
--- OneDay.WebAPI/Program.cs
+++ OneDay.WebAPI/Program.cs
@@ -2,12 +2,16 @@ using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using OneDay.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Add connection string here
 var connectionString = builder.Configuration.GetConnectionString("FKaramdadConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+//Add Floria Service/Interface for Dependency Injection here
+//builder.Services.AddScoped<IFloriaService, FloriaService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
